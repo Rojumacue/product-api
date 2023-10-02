b8939e97c0ed8bcc90ccc2469ec423fe767c49b7
rymartinez <rymartinez@indra.es>
2023-10-02 16:56:08 +0200

    update entity

--- src/main/java/com/product/productapi/domain/Product.java
+++ src/main/java/com/product/productapi/domain/Product.java
@@ -1,10 +1,12 @@
 package com.product.productapi.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Product {
     private String id;
     private String name;
